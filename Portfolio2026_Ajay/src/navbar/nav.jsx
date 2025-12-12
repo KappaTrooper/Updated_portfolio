a4754af95b5829b1ad790ed9ed5c4b47be36bbc4
Ajay Singh <contact@ajaysingh.ca>
2025-12-11 23:11:09 -0500

    Basic Navbar compents added

---
+++ Portfolio2026_Ajay/src/navbar/nav.jsx
@@ -0,0 +1,45 @@
+import './nav.css'
+
+
+export default function Nav() {
+  return (
+
+  <div className='navParent'>
+
+<div className='navItem_name'>
+<h1 className='nav_name'>Ajay Singh</h1>
+<h2 className='nav_title'>Web Developer + Designer</h2>
+
+
+
+</div>
+<div className='navItem_menu'>
+
+    <ul className='nav_Item_menuitems'>
+
+        <li className=''>Work</li>
+        <li className=''>Fun</li>
+        <li className=''>About</li>
+        <li className=''>Resume</li>
+
+
+    </ul>
+
+
+
+</div>
+<div className='navItem_chat'>
+
+    <h1>Coming Soon</h1>
+</div>
+
+
+  </div>
+
+
+)
+
+
+
+
+}
