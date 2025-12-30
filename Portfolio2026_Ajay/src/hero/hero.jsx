b47b782bac22273e4199a0f0a7f0fcf221f96746
Ajay Singh <contact@ajaysingh.ca>
2025-12-29 22:06:15 -0500

    updated nav and working on hero

---
+++ Portfolio2026_Ajay/src/hero/hero.jsx
@@ -0,0 +1,33 @@
+import './hero.css'
+
+
+export default function Nav() {
+  return (
+
+<div className='hero_parents'>
+
+
+<div className='hero_left'>I'm Ajay, a product designer who <span className='hero_span'>Designs</span></div>
+
+<div className='hero_right'></div>
+    <table>
+  <tr>
+    <th>Company</th>
+    <th>Contact</th>
+    <th>Country</th>
+  </tr>
+  <tr>
+    <td>Alfreds Futterkiste</td>
+    <td>Maria Anders</td>
+    <td>Germany</td>
+  </tr>
+  <tr>
+    <td>Centro comercial Moctezuma</td>
+    <td>Francisco Chang</td>
+    <td>Mexico</td>
+  </tr>
+</table>
+</div>
+
+
+  ) }
--- Portfolio2026_Ajay/src/main.jsx
+++ Portfolio2026_Ajay/src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 // import App from './App.jsx'
 import Nav from "./navbar/nav.jsx";
+import Hero from "./hero/hero.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Nav />
+    <Hero />
   </StrictMode>,
 )
